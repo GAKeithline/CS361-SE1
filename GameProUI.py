04fb9fed69986155cdd2b08324f82e3db1657af3
Gilbert Keithline <137833914+GAKeithline@users.noreply.github.com>
2024-05-06 18:50:47 -0700

    Add files via upload

--- GameProUI.py
+++ GameProUI.py
@@ -56,20 +56,20 @@ class GameLib:
         """adds a new game to the library with keywords"""
         new_game = Game()
         print('Are you sure you want to add', new_game.get_name(), 'to the library?')
-        x = input("Enter 'y' to delete, enter any other key to abort: ")
-        if x == 'y':
+        x = input("Enter 'y' to add game, enter any other key to abort: ")
+        if x.lower() == 'y':
             self._library[new_game.get_name()] = new_game
-            print(new_game.get_name(), "has been added to the library! \n")
+            print(new_game.get_name(), "has been added to the library!")
             self._size += 1
         else:
-            print("You have chosen not to add", new_game.get_name(), 'to the library. \n')
+            print("You have chosen not to add", new_game.get_name(), 'to the library.')
 
     def remove_game(self, title):
         """deletes a game from the library"""
         if title in self._library:
             print('Are you sure you want to delete ', title.upper(), ' from the library?')
             x = input("Press 'y' to delete, press any other key to abort: ")
-            if x == 'y':
+            if x.lower() == 'y':
                 del self._library[title]
                 print(title, ' has been deleted from the library')
                 self._size -= 1
@@ -78,39 +78,43 @@ class GameLib:
         else:
             print('There is no game by that title in the library. \n')
 
-close_command = None
+close_command = 'None'
 games = GameLib()
 print('Welcome to GamePro!')
 print('GamePro lets you save all your favorite games in one convenient location and is free to use!')
-while close_command != 'close':
-    print("---------"), print("Home Page"), print("---------")
-    print("What would you like to do? \n")
-    print("Type 'add game' to add a new game to the library.")
-    print("Type 'library' to view, edit, and delete existing games.")
-    x = input("Enter any command other than 'add game' or 'library' to exit: ")
-    if x == "add game":
-        print("---------"), print("Add Game"), print("---------")
+while close_command.lower() != 'close':
+    print("\n---------"), print("Home Page"), print("---------")
+    instruct = input("Enter 'y' to see instructions. Otherwise, simply hit 'enter': ")
+    if instruct.lower() == 'y':
+        print("Type 'add game' to add a new game to the library.")
+        print("Type 'library' to view, edit, and delete existing games.")
+        print("Enter any command other than 'add game' or 'library' to exit. \n")
+    print("What would you like to do?")
+    x = input("Add game, visit library, or exit?: ")
+    if x.lower() == "add game":
+        print("\n---------"), print("Add Game"), print("---------")
         print('Enter "y" to continue adding a game.')
         y = input("Enter any other value to return to the home screen: ")
-        if y == 'y':
+        if y == 'y' or 'Y':
             games.add_game()
-    if x == "library":
-        print("---------"), print("Library"), print("---------")
+    if x.lower() == "library":
+        print("\n---------"), print("Library"), print("---------")
         lib = games.get_keys()
         if lib == []:
-            lib = [None]
-        print('Current titles: ', lib)
-        action = input("Type 'delete' to delete a game or 'view' to see a game's details. Enter any other key to return to the home page: ")
-        if action == 'delete':
+            print('The library is currently empty')
+            action = input("Hit 'enter' to return to the home page: ")
+        else:
+            print('Current titles: ', lib)
+            action = input("Type 'delete' to delete a game or 'view' to see a game's details. Enter any other key to return to the home page: ")
+        if action.lower() == 'delete':
             title = input("Type the name of the game you wish to delete: ")
             games.remove_game(title)
-            print(games.get_keys(), '\n')
-        if action == 'view':
+            print(games.get_keys())
+        if action.lower() == 'view':
             title = input("Please type the name of the game you wish to view and press 'enter': ")
             print('Title: ', games.get_library()[title].get_name()), print('Maker: ', games.get_library()[title].get_maker())
             print('Description: ', games.get_library()[title].get_description()), print('Keywords: ', games.get_library()[title].get_keywords())
-            print('\n')
 
 
-    print('Would you like to continue?')
+    print('\nWould you like to continue?')
     close_command = input("Press any key to return to the Home Page or type 'close' to close the program: ")
